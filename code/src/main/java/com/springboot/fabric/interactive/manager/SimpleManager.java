cf16b670d222cd04fc1ebb15cbd331446b023fbc
Nott <1564223954@qq.com>
2019-01-24 17:42:44 +0800

    update

--- code/src/main/java/com/springboot/fabric/interactive/manager/SimpleManager.java
+++ code/src/main/java/com/springboot/fabric/interactive/manager/SimpleManager.java
@@ -35,34 +35,16 @@ public class SimpleManager extends BaseManager {
 
     private FabricManager obtainFabricManager() throws Exception {
         OrgManager orgManager = new OrgManager();
-        OrgManager orgManager2 = new OrgManager();
-  
-        orgManager2
-        .init("Org1", false, false)
-        .setUser("Admin", getCryptoConfigPath("aberic"), getChannleArtifactsPath("aberic"))
-        .setCA("ca", "http://39.108.64.144:7054")
-        .setPeers("Org1MSP", "org1.example.com")
-        .addPeer("peer0.org1.example.com", "peer0.org1.example.com", "grpc://39.108.64.144:7051", "grpc://39.108.64.144:7053", true)
-        .setOrderers("example.com")
-        .addOrderer("orderer.example.com", "grpc://39.108.64.144:7050")
-        .setChannel("mychannel")
-        .setChainCode("tracegoods", "/opt/gopath", "github.com/hyperledger/fabric/aberic/chaincode/go/trace_goods", "1.0", 90000, 240)
-        .setBlockListener(map -> {
-            logger.debug(map.get("code"));
-            logger.debug(map.get("data"));
-        })
-        .add();
-        
         orgManager
         .init("Org1", false, false)
-        .setUser("Admin", getCryptoConfigPath("sheep"), getChannleArtifactsPath("sheep"))
+        .setUser("Admin", getCryptoConfigPath("aberic"), getChannleArtifactsPath("aberic"))
         .setCA("ca", "http://47.244.142.213:7054")
         .setPeers("Org1MSP", "org1.example.com")
         .addPeer("peer0.org1.example.com", "peer0.org1.example.com", "grpc://47.244.142.213:7051", "grpc://47.244.142.213:7053", true)
         .setOrderers("example.com")
         .addOrderer("orderer.example.com", "grpc://47.244.142.213:7050")
         .setChannel("mychannel")
-        .setChainCode("tracegoods", "/opt/gopath", "github.com/hyperledger/fabric/sheep/chaincode/go/trace_goods", "1.0", 90000, 240)
+        .setChainCode("tracegoods", "/opt/gopath", "github.com/hyperledger/fabric/sheep/chaincode/go/trace_goods", "1.1", 90000, 240)
         .setBlockListener(map -> {
             logger.debug(map.get("code"));
             logger.debug(map.get("data"));
